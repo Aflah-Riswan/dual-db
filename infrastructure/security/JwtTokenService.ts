608b87b647cf9b3a81b8594aad59a7e0f477a9ad
Aflah Riswan <riswanaflah@gmail.com>
2026-06-19 00:22:47 +0530

    added jwt validation

---
+++ infrastructure/security/JwtTokenService.ts
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+import type { ITokenService, IUserPayload } from "../../domain/services/service.token.js";
+
+export class JwtTokenService implements ITokenService {
+ private readonly secret = process.env.JWT_SECRET || 'super_secret_key';
+ 
+generateToken(payload: IUserPayload): string {
+    return jwt.sign(payload, this.secret, { expiresIn: '1d' });
+  }
+
+  verifyToken(token: string): IUserPayload {
+    return jwt.verify(token, this.secret) as IUserPayload;
+  }
+}
