ace7919ef0bd36afb6bd53ca2a1d4549c3d50f3e
Aflah Riswan <riswanaflah@gmail.com>
2026-06-18 23:29:49 +0530

    added syncuser class

---
+++ application/use-cases/SyncUserUseCase.ts
@@ -0,0 +1,18 @@
+import { User } from "../../domain/entities/User.js";
+import type { IUserRepo } from "../../domain/repositories/repo.user.js";
+
+export class SyncUserUserCase {
+  constructor(private readonly mongoRepository: IUserRepo) {}
+  async execute(userData: any): Promise<void> {
+    const userEntity = new User(
+      userData.id,
+      userData.name,
+      userData.email,
+      userData.password,
+      userData.role,
+      userData.isBlocked,
+      new Date(userData.createdAt),
+    );
+    await this.mongoRepository.save(userEntity);
+  }
+}
