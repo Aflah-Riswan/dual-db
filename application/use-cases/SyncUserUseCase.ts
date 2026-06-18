bc4fbff337335341dd94d5d6d7017231b2a1c9d5
Aflah Riswan <riswanaflah@gmail.com>
2026-06-19 00:35:23 +0530

    updated syncuser usecase

--- application/use-cases/SyncUserUseCase.ts
+++ application/use-cases/SyncUserUseCase.ts
@@ -1,17 +1,18 @@
 import { User } from "../../domain/entities/User.js";
-import type { IUserRepo } from "../../domain/repositories/repo.user.js";
+import type { IUserRepository } from "../../domain/repositories/repo.user.js";
 
 export class SyncUserUseCase {
-  constructor(private readonly mongoRepository: IUserRepo) {}
-  async execute(userData: any): Promise<void> {
+  constructor(private readonly mongoRepository: IUserRepository) {}
+
+  async execute(action: string, payload: any): Promise<void> {
+    if (action === 'DELETE') {
+      await this.mongoRepository.delete(payload.id);
+      return;
+    }
+
     const userEntity = new User(
-      userData.id,
-      userData.name,
-      userData.email,
-      userData.password,
-      userData.role,
-      userData.isBlocked,
-      new Date(userData.createdAt),
+      payload.id, payload.name, payload.email, payload.password,
+      payload.role, payload.isBlocked, new Date(payload.createdAt)
     );
     await this.mongoRepository.save(userEntity);
   }
