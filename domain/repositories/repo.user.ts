ab8efa1f3d17ae6ee1d84ac0d927417cb38aea4d
Aflah Riswan <riswanaflah@gmail.com>
2026-06-19 00:29:29 +0530

    updated user repo by adding new crud

--- domain/repositories/repo.user.ts
+++ domain/repositories/repo.user.ts
@@ -1,7 +1,10 @@
 import type { User } from "../entities/User.js";
 
-export interface IUserRepo {
+export interface IUserRepository {
   save(user: User): Promise<User>;
+  update(id: number, user: Partial<User>): Promise<User>;
+  delete(id: number): Promise<void>;
+  findById(id: number): Promise<User | null>;
   findByEmail(email: string): Promise<User | null>;
   findAll(): Promise<User[]>;
 }
