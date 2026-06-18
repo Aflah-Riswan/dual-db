b55b7943f750bfae89bfb9f8fbaf9fc30e06fea8
Aflah Riswan <riswanaflah@gmail.com>
2026-06-19 00:32:48 +0530

    updated createuser usecase

--- application/use-cases/CreateUserCase.ts
+++ application/use-cases/CreateUserCase.ts
@@ -1,22 +1,29 @@
 import { User } from "../../domain/entities/User.js";
-import type { IUserRepo } from "../../domain/repositories/repo.user.js";
+import type { IUserRepository } from "../../domain/repositories/repo.user.js";
 
-export interface IMessegingService {
-  publish(queue: string, message: any): Promise<void>;
+import type { IEncryptionService } from "../../domain/services/IEncryptionService.js";
+
+export interface IMessagingService {
+  publish(queue: string, action: string, message: any): Promise<void>;
 }
 
 export class CreateUserUseCase {
   constructor(
-    private readonly sqlRepo: IUserRepo,
-    private readonly messageService: IMessegingService,
+    private readonly sqlRepository: IUserRepository,
+    private readonly messagingService: IMessagingService,
+    private readonly encryptionService: IEncryptionService
   ) {}
-  async execute(userData: Omit<User, "id" | "role" | "isBlocked" | "createdAt">): Promise<User> {
-    const exisitingUser = await this.sqlRepo.findByEmail(userData.email);
-    if (exisitingUser) throw new Error("User already existed");
-    const userEntity = new User(null, userData.name, userData.email, userData.password);
-    const savedUser = this.sqlRepo.save(userEntity);
 
-    await this.messageService.publish("user_sync_queue", savedUser);
+  async execute(userData: Omit<User, 'id' | 'role' | 'isBlocked' | 'createdAt'> & { role?: string }): Promise<User> {
+    const existingUser = await this.sqlRepository.findByEmail(userData.email);
+    if (existingUser) throw new Error('User already exists');
+
+    const hashedPassword = await this.encryptionService.hash(userData.password!);
+    
+    const userEntity = new User(null, userData.name, userData.email, hashedPassword, userData.role || 'user');
+    const savedUser = await this.sqlRepository.save(userEntity);
+
+    await this.messagingService.publish('user_sync_queue', 'CREATE', savedUser);
     return savedUser;
   }
 }
